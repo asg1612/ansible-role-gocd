d32a5a8b9fea869ed9674f9fa9e68755db0481b2
Andrés Sánchez García <asg1612@gmail.com>
2018-02-09 14:13:41 +0100

    Add tests fix #1

--- molecule/default/tests/test_agent.py
+++ molecule/default/tests/test_agent.py
@@ -3,7 +3,7 @@ import os
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('molecule-gocd-agent')
 
 
 def test_hosts_file(host):
@@ -12,3 +12,8 @@ def test_hosts_file(host):
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+def test_agent_service(host):
+        service = host.service("go-agent")
+        assert(service.is_running)
+        assert(service.is_enabled)
